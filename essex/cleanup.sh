3579a62be61175c375c53a8073d1e8fe2ea6ce2d
hvnsweeting <hvnsweeting@gmail.com>
2013-05-08 10:10:35 +0700

    cleanup keystone glance nova

--- essex/cleanup.sh
+++ essex/cleanup.sh
@@ -1,9 +1,12 @@
 # drop db
 PASSWD=openstack
-for u in 'keystone' 'glance'; do
+for u in 'keystone' 'glance' 'nova'; do
 	mysql -u $u -p$PASSWD -e "drop database $u" $u
 done
 
 # remove soft
-apt-get purge keystone -y
-rm -rf /etc/keystone/
+apt-get purge 'nova-*' glance-common keystone -y
+set -x
+rm -rf /var/lib/{nova,glance,keystone}
+rm -rf /etc/{keystone,nova,glance}
+set +x
