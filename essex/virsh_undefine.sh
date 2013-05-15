334bcce00679096c0f6a1436897f3c4abc8474c0
hvnsweeting <hvnsweeting@gmail.com>
2013-05-15 09:26:13 +0700

    add option for remove when have err

--- essex/virsh_undefine.sh
+++ essex/virsh_undefine.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 for i in `virsh list --all | grep instance | awk '{print $2}'`; do
-    virsh destroy "$i"; virsh undefine "$i"
+    virsh destroy "$i"
+    virsh undefine --managed-save "$i"
 done
