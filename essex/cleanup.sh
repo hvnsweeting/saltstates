2bab0369672643c6b347fcf528b4fa6098468a21
hvnsweeting <hvnsweeting@gmail.com>
2013-05-17 01:37:43 +0700

    add rm ins

--- essex/cleanup.sh
+++ essex/cleanup.sh
@@ -1,4 +1,9 @@
 # drop db
+for i in `virsh list --all | grep instance | awk '{print $2}'`; do
+    virsh destroy "$i"
+    virsh undefine --managed-save "$i"
+done
+
 PASSWD=openstack
 for u in 'keystone' 'glance' 'nova'; do
 	mysql -u $u -p$PASSWD -e "drop database $u" $u
@@ -9,4 +14,5 @@ apt-get purge 'nova-*' glance-common keystone -y
 set -x
 rm -rf /var/lib/{nova,glance,keystone}
 rm -rf /etc/{keystone,nova,glance}
+
 set +x
